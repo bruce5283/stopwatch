35dfed1b881ef54c45af892285935c3f9cea6c5b
bruce5283 <bcarrington7@gmail.com>
2022-12-19 11:02:12 -0500

    Updated

--- function.js
+++ function.js
@@ -12,131 +12,117 @@ window.function = function (seq) {
   let ht = `<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Glide Yes-Code</title>
+    <title>JavaScript StopWatch</title>
+    <style> 
+    #stopwatch-container {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      margin: 100px auto 0;
+      width: fit-content;
+      padding: 10px 20px;
+      box-shadow: 1px 0px 4px 2px #9c9a9a;
+      border-radius: 5px;
+    }
+ 
+    #stopwatch {
+      margin: 0 auto;
+      text-align: center;
+      font-size: 60px;
+    }
+ 
+    #buttons-container {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+    }
+ 
+    #buttons-container button {
+      outline: none;
+      cursor: pointer;
+      color: #fff;
+      background-color:#000;
+      border: none;
+      border-radius: 5px;
+      font-size: 20px;
+      margin: 0 10px;
+      padding: 3px 8px;
+    }
+ 
+    #buttons-container button:active {
+      opacity: 0.7;
+    }
+  </style>
   </head>
   <body>
-   <!-- Display the stopwatch in an element -->
-<div class = "container">
-    <p id="stopwatch">00:00:00:00</p>
-        <div id="buttons-container">
-            <button onclick="main()" id="main-btn" class="btn">Start</button>
-            <button onclick="reset()" id="reset-btn" class="btn">Reset</button>
+    <div id="stopwatch-container">
+      <p id="stopwatch">00:00:00:00</p>
+      <div id="buttons-container">
+        <button onclick="main()" id="main-btn" class="btn">Start</button>
+        <button onclick="reset()" id="reset-btn" class="btn">Reset</button>
+      </div>
     </div>
-</div>
-<style>
-
-#container {
-text-align: center;
-width: ${width}vw;
-height:${height}vh;
-}
-
-#buttons-container {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
-#buttons-container button {
-outline: none;
-cursor: pointer;
-color: #fff;
-background-color: #000;
-border: none;
-border-radius: none;
-font-size: 15px;
-margin: 0 10px;
-padding: 3px 8px;
-
-}
-
-#buttons-container button:active {
-  opacity: 0.7;
-}
-
-#stopwatch {
-font-weight: ${fweight};
-text-align: ${align};
-font-family: verdana;
-font-size: ${fsize}px;
-}
-
-@media (prefers-color-scheme: dark) {
-
-#stopwatch {
-color: #12A89E;
-}
-}
-
-@media (prefers-color-scheme: light) {
-
-#stopwatch {
-color: #12A89E;
-}
-}
-
-</style>
-<script>
-// Set sequence
-let seq = ${seq};
-//Global variables
-var stopwatch = document.getElementById("stopwatch");
-var mainButton = document.getElementById("main-btn");
-var startTime = 0;
-var elapsedTime = 0;
-var timeoutID = null;
-
-//Start / stop function
-function main(){
-    stopwatch.innerHTML = "Failed";
-    if(mainButton.innerHTML.trim() === "Start"){
-        startTime = Date.now();
-            stopwatch.innerHTML = "Passed";
-            startStopwatch();
-            mainButton.innerHTML = "Stop");
-    } else{
-        elapsedTime += Date.now() -startTime;
-        stopwatch.innerHTML = "Failed";
-        clearTimeout(timeoutID);
-        mainButton.innerHTML = "Start";
-    }
-}
-
-//reset stopwatch
-function reset(){
-    elapsedTime =0;
-    startTime = Date.now();
-    clearTimeout(timeoutID);
-    mainButton.innerHTML = "Start";
-    displayTime(0,0,0,0);
-}
-
-//start the stopwatch
-function startStopwatch(){
-    timeoutID = setTimeout(function(){
-        const time = Date.now()-startTime + elapsedTime;
-        const milliseconds = parseInt((time%1000)/10);
-        const seconds = parseInt((time/1000)%60);
-        const minutes = parseInt((time/(1000*60))%60);
-        const hour = parseInt((time/(1000*60*60))%24);
-        
-        displayTime(hour, minutes, seconds, milliseconds);
-        
+    <script>
+    //globle variables
+    var stopwatch = document.getElementById('stopwatch');
+    var mainButton = document.getElementById('main-btn');
+    var startTime=0;
+    var elapsedTime=0;
+    var timeoutId=null;
+ 
+    //method to operate start and stop function
+    function main(){
+      if (mainButton.innerHTML.trim() === 'Start') {
+      startTime = Date.now();
         startStopwatch();
-    }, 100);
-}
-
-//display time in format
-function displayTime(hour, minutes, seconds, milliseconds){
-    hour = hour < 10 ? '0' +hour : hour;
-    minutes = minutes < 10 ? '0' +minutes : minutes;
-    seconds = seconds < 10 ? '0' +seconds : seconds;
-    milliseconds = milliseconds < 10 ? '0' +milliseconds : milliseconds;
-    stopwatch.innerHTML = hour +':' + minutes +':' + seconds + ':'+ milliseconds;
-}
-
-</script>
+        mainButton.innerHTML = 'Stop';
+      } else {
+        elapsedTime += Date.now() - startTime;
+        clearTimeout(timeoutId);
+        mainButton.innerHTML = 'Start';
+      }
+    }
+ 
+    //method to reset the stopwatch
+    function reset() {
+      elapsedTime = 0;
+      startTime = Date.now();
+      clearTimeout(timeoutId);
+      mainButton.innerHTML = 'Start';
+      displayTime(0, 0, 0, 0);  
+    }
+ 
+    //method to start the stopwatch
+    function startStopwatch() {
+      
+      //run setTimeout() and save id
+      timeoutId = setTimeout(function(){
+        //calculate elapsed time
+        const time = Date.now() - startTime + elapsedTime;
+         
+        //calculate different time measurements based on elapsed time
+        const milliseconds = parseInt((time%1000)/10)
+        const seconds = parseInt((time/1000)%60)
+        const minutes = parseInt((time/(1000*60))%60)
+        const hour = parseInt((time/(1000*60*60))%24);
+         
+        //display time
+        displayTime(hour, minutes, seconds, milliseconds);
+         
+        //calling same method again recursivaly
+        startStopwatch();
+      }, 100);
+    }
+ 
+    //method to display time in '00:00:00:00' format
+    function displayTime(hour, minutes, seconds, milliseconds) {
+        hour = hour < 10 ? '0'+hour : hour ;
+        minutes = minutes < 10 ? '0'+minutes : minutes ;
+        seconds = seconds < 10 ? '0'+seconds : seconds ;
+        milliseconds = milliseconds < 10 ? '0'+milliseconds : milliseconds ;
+        stopwatch.innerHTML = hour+':'+minutes+':'+seconds+':'+milliseconds;
+    }
+    </script>
   </body>
 </html>`
 
